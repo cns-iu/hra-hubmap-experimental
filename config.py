4a01b87e8ad21b532eec70a837d07479b70e7a6f
Aashay Gondalia <aagond@iu.edu>
2023-03-17 14:45:15 -0400

    create generate tables notebook

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 configs = {
     "output_filename" : "hubmap_metadata",
-    "max_requests_per_sec" : 50,
-    "nexus_token" : "AggwwXaV9ONlvNzyNXngPEV5KynybYp7b5NzPlnG03YrVX52aXFJCW4waB7oNQODO8r8xl1QzNwOPjs0VyMXnfmNJ5",
+    "max_requests_per_sec" : 100,
+    "nexus_token" : "AgemlnWQ7XznjmkX3Px52Om84DYkzK1VP0Nmyw5eJ9NkkdO7EXSVCyWEWxJ4KWPg7Qzz5ozDB7JP3BU5xp8pvtzerY",
 }
